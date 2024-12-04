aeaffa1c19418a3d1ba362242b183eb7bb9ea868
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-12-04 23:57:57 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="text-3xl">Landing page</div>
+    </div>
+  );
+};
+
+export default page;
